da839d57a9c3d43d22267e18e5c8ea81531e409e
unknown <zhuhuotui@gmail.com>
2016-12-28 11:29:08 +0800

    refactor:delete unuseful code

--- main.cpp
+++ main.cpp
@@ -151,27 +151,6 @@ int CALLBACK WinMain(
 	_In_ int       nCmdShow
 ) {
 
-	HANDLE hToken;
-	LUID sedebugnameValue;
-	TOKEN_PRIVILEGES tkp;
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
-	{
-		return -1;
-	}
-	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &sedebugnameValue))
-	{
-		CloseHandle(hToken);
-		return -1;
-	}
-	tkp.PrivilegeCount = 1;
-	tkp.Privileges[0].Luid = sedebugnameValue;
-	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-	if (!AdjustTokenPrivileges(hToken, FALSE, &tkp, sizeof(tkp), NULL, NULL))
-	{
-		CloseHandle(hToken);
-		return -1;
-	}
-
 	MSG msg;
 
 	WNDCLASSEX wcx;
